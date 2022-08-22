400b70231471c941ee1ae46507d04dd5be7497db
Mudryi <42421131+Mudryi@users.noreply.github.com>
2022-08-23 00:54:13 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.utils_data import read_and_transform_data
+
+data = read_and_transform_data('data/sum_12_full_try6.jsonlines')
+
+print(data.head())
---
+++ src/utils_data.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+MIN_LEMMA_LENTH = 3
+
+
+def read_and_transform_data(path):
+    data = pd.read_json(path, lines=True)
+
+    data = data[data.lemma.apply(len) > MIN_LEMMA_LENTH]
+
+    data = data.explode('synsets')
+    data.dropna(subset=['synsets'], inplace=True)
+
+    data = data[data['synsets'].apply(lambda x: len(x['gloss'])) == 1]
+    data = data[data['synsets'].apply(lambda x: len(x['examples'])) > 0]
+
+    data = pd.concat([data.lemma, data.synsets.apply(pd.Series)], axis=1)
+    data.drop(columns=['sense_id'], inplace=True)
+    data['gloss'] = data['gloss'].apply(lambda x: x[0])
+    data['examples'] = data['examples'].apply(lambda x: x[0]['ex_text'])
+
+    gloss_to_remove = data.groupby("gloss").filter(lambda x: len(x) > 1)["gloss"].tolist()
+    data = data[~data["gloss"].isin(gloss_to_remove)]
+
+    data = data.groupby("lemma").filter(lambda x: len(x) > 1)
+    return data
