19560cbcdb4b0540057cce737669cea5934924d6
Mudryi <42421131+Mudryi@users.noreply.github.com>
2022-08-30 22:37:40 +0300

    - change generate_results_by_pos to generate results for all pos - add function generate_results_by_gloss to generate results by words which have different numbers of occurrence

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import pandas as pd
 from src.utils_data import read_and_transform_data
-from src.utils_results import generate_results_df
+from src.utils_results import results_reports
 
 # data = read_and_transform_data('data/sum_12_full_try6.jsonlines')
 # predicted_data = prediction_function(data)
 predicted_data = pd.read_csv('data/dummy_prediction.csv')
-print(generate_results_df(predicted_data))
-
+results_reports(predicted_data)
--- src/utils_results.py
+++ src/utils_results.py
@@ -2,6 +2,9 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 import pymorphy2
 
+MINIMUM_POS_OCCURRENCE = 100
+MINIMUM_GLOSS_OCCURRENCE = 300
+
 
 def prediction_accuracy(data_with_predictions):
     data_dropna = data_with_predictions.dropna()
@@ -26,13 +29,43 @@ def add_pos_tag(data_with_predictions):
     return data_with_predictions
 
 
-def generate_results_df(data_with_predictions):
+def generate_results_by_pos(data_with_predictions):
     data_with_predictions = add_pos_tag(data_with_predictions)
-    data_with_predictions_nouns = data_with_predictions[data_with_predictions["pos"] == "NOUN"]
-    data_with_predictions_verb = data_with_predictions[data_with_predictions["pos"] == "VERB"]
-    results_df = pd.DataFrame.from_dict({'overall_accuracy': prediction_accuracy(data_with_predictions),
-                                         'noun_accuracy': prediction_accuracy(data_with_predictions_nouns),
-                                         'verb_accuracy': prediction_accuracy(data_with_predictions_verb)},
+
+    results = {'overall_accuracy': [prediction_accuracy(data_with_predictions), len(data_with_predictions)]}
+
+    for pos in data_with_predictions["pos"].value_counts().iteritems():
+        if pos[1] < MINIMUM_POS_OCCURRENCE:
+            break
+        data_with_predictions_pos = data_with_predictions[data_with_predictions["pos"] == pos[0]]
+        results[pos[0]+'_accuracy'] = [prediction_accuracy(data_with_predictions_pos), len(data_with_predictions_pos)]
+    results_df = pd.DataFrame.from_dict(results,
                                         orient='index',
-                                        columns = ['accuracy'])
+                                        columns=['accuracy', 'count'])
     return results_df
+
+
+def generate_results_by_gloss(data_with_predictions):
+    gloss_count = data_with_predictions.groupby('lemma').gloss.count().to_dict()
+    data_with_predictions['gloss_count'] = data_with_predictions['lemma'].map(gloss_count)
+
+    results = {'overall_accuracy': [prediction_accuracy(data_with_predictions), len(data_with_predictions)]}
+
+    for gloss_count in data_with_predictions["gloss_count"].value_counts().sort_index().iteritems():
+        if gloss_count[1] < MINIMUM_GLOSS_OCCURRENCE:
+            data_with_predictions_gloss = data_with_predictions[data_with_predictions["gloss_count"] >= gloss_count[0]]
+            results[str(gloss_count[0]) + '+_gloss_word_accuracy'] = [prediction_accuracy(data_with_predictions_gloss),
+                                                                      len(data_with_predictions_gloss)]
+            break
+        data_with_predictions_gloss = data_with_predictions[data_with_predictions["gloss_count"] == gloss_count[0]]
+        results[str(gloss_count[0])+'_gloss_word_accuracy'] = [prediction_accuracy(data_with_predictions_gloss),
+                                                               len(data_with_predictions_gloss)]
+    results_df = pd.DataFrame.from_dict(results,
+                                        orient='index',
+                                        columns=['accuracy', 'count'])
+    return results_df
+
+
+def results_reports(data_with_predictions):
+    print(generate_results_by_pos(data_with_predictions))
+    print(generate_results_by_gloss(data_with_predictions))
