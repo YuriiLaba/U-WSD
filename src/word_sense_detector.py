6b4c4d31642935e0cc60c55635da1c8781919abb
yurii_l <ylaba@intelliarts.com>
2022-09-03 15:23:48 +0300

    Added WordSenseDetector v1

---
+++ src/word_sense_detector.py
@@ -0,0 +1,171 @@
+import torch
+import numpy as np
+
+from transformers import AutoTokenizer, AutoModel
+
+from collections import Counter
+from scipy.spatial import distance
+
+
+class WordSenseDetector:
+
+    def __init__(self, pretrained_model, udpipe_model, evaluation_dataset):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = AutoModel.from_pretrained(pretrained_model, output_hidden_states=True).to(self.device)
+        self.tokenizer = AutoTokenizer.from_pretrained(pretrained_model)
+        self.udpipe_model = udpipe_model
+        self.evaluation_dataset = evaluation_dataset
+        self.missing_target_word_in_sentence = 0
+
+    def tokenize_text(self, input_text):
+        return self.tokenizer(input_text, return_tensors='pt').to(self.device)
+
+    def find_target_word_in_sentence(self, input_text, target_word):
+        """
+        TODO: This function wil only find one target word
+        """
+
+        tokenized = self.udpipe_model.tokenize(input_text)
+
+        for tok_sent in tokenized:
+
+            self.udpipe_model.tag(tok_sent)
+
+            for word_index, w in enumerate(tok_sent.words[1:]):  # under 0 index is root
+                # print(w.lemma)
+
+                if w.lemma.lower() == target_word.lower():
+                    return [w.form for w in tok_sent.words[1:]][word_index]
+
+        self.missing_target_word_in_sentence += 1
+        return None
+
+    def find_target_word_in_tokenized_text(self, tokenized_input_text, word):
+        unique_w_ids = list(Counter(tokenized_input_text.word_ids()).keys())
+        unique_w_ids.remove(None)
+        tokens = ["" for i in range(len(unique_w_ids))]
+
+        word_indexes = []
+        prev_word_id = None
+
+        target_words_with_indexes = []
+
+        # for index, (token, word_id) in enumerate(zip(self.tokenizer.convert_ids_to_tokens(tokenized_input_text.input_ids[0]), tokenized_input_text.word_ids())):
+
+        for index, (input_id, word_id) in enumerate(zip(tokenized_input_text["input_ids"][0],
+                                                        tokenized_input_text.word_ids())):
+            token = self.tokenizer.decode([input_id]).strip()
+
+            if prev_word_id == word_id:
+                word_indexes.append(index)
+
+            else:
+                word_indexes = []
+                word_indexes.append(index)
+
+            prev_word_id = word_id
+
+            if word_id is None:
+                continue
+
+            if token.startswith("##"):
+                token = token.replace("##", "")
+
+            tokens[word_id] += token
+            if tokens[word_id].replace("▁", "").lower() == word.lower():
+                target_words_with_indexes.append((word, word_indexes.copy()))
+        # print(tokens)
+
+        return target_words_with_indexes
+
+    def run_inference(self, tokenized_input_text):
+        with torch.no_grad():
+            model_output = self.model(**tokenized_input_text)
+        return model_output
+
+    def get_hidden_states(self, model_output):
+        hidden_states = model_output["hidden_states"]
+
+        hidden_states = torch.stack(hidden_states, dim=0)
+        hidden_states = torch.squeeze(hidden_states, dim=1)
+        hidden_states = hidden_states.permute(1,0,2)
+
+        return hidden_states
+
+    def get_last_hidden_state(self, hidden_states):
+        return hidden_states[:, -1]
+
+    def mean_pooling(self, model_output, attention_mask):
+        last_hidden_state = model_output[0] # last hidden state
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(last_hidden_state.size()).float()
+        return (torch.sum(last_hidden_state * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)).cpu().detach().numpy()
+
+    def get_embedding_of_the_target_word(self, hidden_states, target_word_idexes):
+        final_embedding = []
+
+        last_hidden_state = self.get_last_hidden_state(hidden_states).cpu().detach().numpy()
+
+        for idx, embedding in enumerate(last_hidden_state):
+            if idx in target_word_idexes:
+                final_embedding.append(embedding)
+
+        return np.asarray(final_embedding).mean(axis=0)
+
+    def predict_word_sense(self, row):
+        ACUTE = chr(0x301)
+        GRAVE = chr(0x300)
+
+        lemma = row["lemma"]
+        example = row["example"]
+
+        lemma_fixed = lemma.replace(GRAVE, "").replace(ACUTE, "")
+
+
+        word = self.find_target_word_in_sentence(example, lemma_fixed)
+        if word == None:
+            # print("Can't find target word in sentence")
+            return None
+
+        tokenized_input_text = self.tokenize_text(example)
+        word_in_tokens = self.find_target_word_in_tokenized_text(tokenized_input_text, word)
+
+
+        if len(word_in_tokens) == 0:
+            # print("Cant find target word in tokens")
+            return None
+        if len(word_in_tokens) > 1:
+            # print("Skip for now")
+            return None
+
+        model_output = self.run_inference(tokenized_input_text)
+        hidden_states = self.get_hidden_states(model_output)
+
+        contexts = self.evaluation_dataset[self.evaluation_dataset["lemma"] == lemma]["gloss"].tolist()
+
+        max_sim = -1
+        correct_context = None
+
+        for word_info in word_in_tokens:
+            word_ = word_info[0]
+            word_indexes_ = word_info[1]
+
+            word_embedding = self.get_embedding_of_the_target_word(hidden_states, word_indexes_)
+
+            for context in contexts:
+
+                tokenized_input_text_context = self.tokenize_text(context)
+
+                model_output_context = self.run_inference(tokenized_input_text_context)
+
+                context_embedding = self.mean_pooling(model_output_context, tokenized_input_text_context['attention_mask'])
+
+                similarity = 1 - distance.cosine(word_embedding, context_embedding)
+                if similarity > max_sim:
+                    max_sim = similarity
+                    correct_context = context
+
+        return correct_context
+
+    def run(self):
+        self.evaluation_dataset["predicted_context"] = self.evaluation_dataset.apply(self.predict_word_sense, axis=1)
+        return self.evaluation_dataset
