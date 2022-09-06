cdda42f093bc35f252575e82fec8ae8445c90985
yurii_l <ylaba@intelliarts.com>
2022-09-06 09:09:34 +0300

    Added concatenate pooling, last four sum pooling, last two sum pooling and conv 1d pooling implementation

--- src/word_sense_detector.py
+++ src/word_sense_detector.py
@@ -104,7 +104,6 @@ class WordSenseDetector:
         return torch.max(last_hidden_state, dim=0).values.cpu().detach().numpy()
 
     def mean_max_pooling(self, hidden_states):
-
         last_hidden_state = self.get_last_hidden_state(hidden_states)
 
         mean_pooling_embeddings = torch.mean(last_hidden_state, dim=0)
@@ -112,8 +111,40 @@ class WordSenseDetector:
 
         return torch.cat((mean_pooling_embeddings, max_pooling_embeddings), dim=0).cpu().detach().numpy() # TODO: check about dim 0
 
+    def concatenate_pooling(self, hidden_states):
+        last_four_layers = [hidden_states[:, i] for i in (-1, -2, -3, -4)]
+        cat_hidden_states = torch.cat(last_four_layers, -1)
+        cat_hidden_states = torch.reshape(torch.mean(cat_hidden_states, axis=0), (1, -1))
+        return cat_hidden_states.cpu().detach().numpy()
+
+    def last_four_sum_pooling(self, hidden_states):
+        last_four_states = torch.sum(hidden_states[:, -4:], dim=1)
+
+        return torch.mean(last_four_states, dim=0).cpu().detach().numpy()
+
+    def last_two_sum_pooling(self, hidden_states):
+        last_two_states = torch.sum(hidden_states[:, -2:], dim=1)
+
+        return torch.mean(last_two_states, dim=0).cpu().detach().numpy()
+
+    def conv_1d_pooling(self, hidden_states):
+        last_hidden_state = self.get_last_hidden_state(hidden_states)
+        last_hidden_state.unsqueeze_(0)
+
+        last_hidden_state = last_hidden_state.cpu().detach()
+
+        cnn1 = torch.nn.Conv1d(768, 256, kernel_size=2, padding=1)
+        cnn2 = torch.nn.Conv1d(256, 1, kernel_size=2, padding=1)
+
+        last_hidden_state = last_hidden_state.permute(0, 2, 1)
+        cnn_embeddings = torch.nn.functional.relu(cnn1(last_hidden_state))
+
+        return torch.mean(cnn2(cnn_embeddings)[0]).detach().numpy()
+
+
     def get_embedding_of_the_target_word(self, hidden_states, target_word_indexes):
         return self.mean_pooling(hidden_states[target_word_indexes[0]:target_word_indexes[-1]+1])
+
 
     def predict_word_sense(self, row):
         ACUTE = chr(0x301)
@@ -163,6 +194,7 @@ class WordSenseDetector:
                 context_embedding = self.mean_pooling(hidden_states_context)
 
                 similarity = 1 - distance.cosine(word_embedding, context_embedding)
+
                 if similarity > max_sim:
                     max_sim = similarity
                     correct_context = context
