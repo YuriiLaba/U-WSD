e767dcdfbb0a31811a869a1f1addb673a09d4f98
Mudryi <42421131+Mudryi@users.noreply.github.com>
2023-04-22 00:11:54 +0300

    - add LEMMAS_TO_REMOVE

--- src/config.py
+++ src/config.py
@@ -3,6 +3,8 @@ MIN_LEMMA_LENTH = 3
 MAX_GLOSS_OCCURRENCE = 4
 ACUTE = chr(0x301)
 GRAVE = chr(0x300)
+LEMMAS_TO_REMOVE = ['або', 'ага', 'адже', 'але', 'ану', 'ані', 'вона', 'еге', 'летяга', 'лише', 'мирно', 'немовби',
+                    'нерозкладний', 'нехай', 'ніби', 'нібито', 'отже', "коли", "отож", "геть", "єсть"]
 
 # [RESULTS]
 MINIMUM_POS_OCCURRENCE = 100
@@ -15,4 +17,3 @@ PATH_TO_SOURCE_UDPIPE = "20180506.uk.mova-institute.udpipe"
 PATH_TO_SAVE_GATHERED_DATASET = "./lemma_examples.json"
 PATH_TO_LEMMAS_OF_INTEREST = "unique_lemmas_homonyms.txt"
 NUMBER_OF_EXAMPLES_TO_GATHER = 10000
-
