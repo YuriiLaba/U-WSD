1b3f1448c579622064729be6afd1258336c73211
Mudryi <42421131+Mudryi@users.noreply.github.com>
2023-04-21 19:14:40 +0300

    - increase MAX_GLOSS_OCCURRENCE to 4 - move lemmas to remove in config - add remove_reference_lemma flag in prepare_dataset - revrite some code to be in functions

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import pandas as pd
-from src.utils_data import read_and_transform_data, prepare_frequent_dictionary
+from src.utils_data import read_and_transform_data
 from src.utils_results import results_reports
 from src.word_sense_detector import WordSenseDetector
 from src.udpipe_model import UDPipeModel
 import warnings
 warnings.filterwarnings('ignore')
 
-# prepare_frequent_dictionary('data/ubertext.fiction_news_wikipedia.filter_rus+short.csv.xz')
 data = read_and_transform_data('data/sum_12_fixed.jsonlines', homonym=True)
 
 udpipe_model = UDPipeModel("data/20180506.uk.mova-institute.udpipe")
--- src/config.py
+++ src/config.py
@@ -1,8 +1,10 @@
 # [PREPARATION]
 MIN_LEMMA_LENTH = 3
-MAX_GLOSS_OCCURRENCE = 3
+MAX_GLOSS_OCCURRENCE = 4
 ACUTE = chr(0x301)
 GRAVE = chr(0x300)
+LEMMAS_TO_REMOVE = ['або', 'ага', 'адже', 'але', 'ану', 'ані', 'вона', 'еге', 'летяга', 'лише', 'мирно', 'немовби',
+                    'нерозкладний', 'нехай', 'ніби', 'нібито', 'отже', "коли", "отож", "геть", "єсть"]
 
 # [RESULTS]
 MINIMUM_POS_OCCURRENCE = 100
